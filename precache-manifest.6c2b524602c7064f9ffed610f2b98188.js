a9edfa81f7dd499296ee126f4e8013bd7a177322
Jesse Walter Vanderwerf <jessev@Jesses-MacBook-Pro.local>
2020-04-23 17:43:12 -0700

    Updates

---
+++ precache-manifest.6c2b524602c7064f9ffed610f2b98188.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e3edac35e3fbe6a43e5681fb43e0a5",
+    "url": "/stock-twit-app/index.html"
+  },
+  {
+    "revision": "03dcbd9825d6531e2dcd",
+    "url": "/stock-twit-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f9840ac16871fea7373",
+    "url": "/stock-twit-app/static/js/2.0abefe94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-twit-app/static/js/2.0abefe94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dcbd9825d6531e2dcd",
+    "url": "/stock-twit-app/static/js/main.1dfb3b96.chunk.js"
+  },
+  {
+    "revision": "43baa6328cfb5ea38af9",
+    "url": "/stock-twit-app/static/js/runtime-main.91bf2f14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock-twit-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
